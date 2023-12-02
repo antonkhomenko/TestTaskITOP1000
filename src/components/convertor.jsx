1f12d4a1e66ca5c27436036c358f651d5a76930c
antonkhomenko <antonkhomenko5@gmail.com>
2023-12-02 19:40:07 +0200

    init test branch

--- src/components/convertor.jsx
+++ src/components/convertor.jsx
@@ -11,7 +11,6 @@ const Convertor = () => {
   //   getExchangeRate("EUR", 1).then(data => setConvertRate(data));
   // }, []);
 
-  test my git 
 
   return (
     <div className="w-full  m-[0_auto] p-8 flex justify-around bg-gray-500 rounded-md">
